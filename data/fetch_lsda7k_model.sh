c08f501f3e53f8b04af0b64216a0ea911fbdf054
Judy Hoffman <jhoffman@durian4.(none)>
2014-11-02 14:37:55 -0800

    fixing the fetch command

--- data/fetch_lsda7k_model.sh
+++ data/fetch_lsda7k_model.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-mkdir data/rcnn_models
-
 echo "Downloading LSDA 7K model..."
 
-wget https://www.eecs.berkeley.edu/~jhoffman/caffe_nets/rcnn_model7200.mat
+wget --no-check-certificate https://www.eecs.berkeley.edu/~jhoffman/caffe_nets/rcnn_model7200.mat \
+--output-document=data/rcnn_models/rcnn_model7200.mat
+
+echo "Downloading R-CNN initial weights..."
+
+wget --no-check-certificate \
+https://www.eecs.berkeley.edu/~jhoffman/caffe_nets/finetune_ilsvrc13_val1+train1k_iter_50000 \
+--output-document=data/caffe_nets/finetune_ilsvrc13_val1+train1k_iter_50000
 
 echo "Done."
